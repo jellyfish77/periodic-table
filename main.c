d230b85e9332d410dbaf35251862273f144466f8
jellyfish77 <jellyfish77@gmx.com>
2022-04-15 15:26:15 +0800

    feat: add func to check if record exists

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 // Structure of the element
 struct Element
 {
-    char symbol[2];
+    char symbol[3];
     char name[50];
     int atomic_no;
     float atomic_wt;
@@ -15,8 +15,10 @@ struct Element
 // function declerations
 void print_record(struct Element *e);
 FILE *open_file(char *filename);
+bool record_exists(char *element_name, FILE *fp);
 
 #define ELEMENTS_FILE ("elements.csv")
+#define SEPERATOR (',')
 
 /**
  * Add an element record if it doesn't exist (by symbol).
@@ -29,18 +31,20 @@ FILE *open_file(char *filename);
  */
 int add_record(struct Element *e, FILE *fp)
 {
-    print_record(e);
-
+    if(record_exists(e->symbol, fp))
+    {
+        return true;
+    }
+    //print_record(e);
     //FILE *fp = open_file(ELEMENTS_FILE);
-
-    printf("Position of file pointer is : ");
-    printf("%ld \n", ftell(fp));
+    //printf("Position of file pointer is : ");
+    //printf("%ld \n", ftell(fp));
     fseek(fp, 0, SEEK_END); // move to end of file
-    printf("Position of file pointer is : ");
-    printf("%ld \n", ftell(fp));
-    fprintf(fp,"%s, %s, %d, %.6f\n", e->symbol, e->name, e-> atomic_no, e->atomic_wt);
-    printf("Position of file pointer is : ");
-    printf("%ld \n", ftell(fp));
+    //printf("Position of file pointer is : ");
+    //printf("%ld \n", ftell(fp));
+    fprintf(fp,"%s,%s,%d,%.6f\n", e->symbol, e->name, e-> atomic_no, e->atomic_wt);
+    //printf("Position of file pointer is : ");
+    //printf("%ld \n", ftell(fp));
     //fwrite(&e)
     return true;
 }
@@ -55,10 +59,38 @@ bool modify_record()
 {
 }
 
-bool record_exists(struct Element *e, FILE *fp)
+const char* getfield(char* line, int num)
 {
-    fseek(fp, 0, SEEK_END); // move to end of file
-    return false;
+    const char* tok;
+    for (tok = strtok(line, ",");
+            tok && *tok;
+            tok = strtok(NULL, ",\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
+bool record_exists(char *element_name, FILE *fp)
+{
+    rewind(fp); // move to start of file
+    bool result = false;
+
+    char line[1024];
+    while (fgets(line, 1024, fp))
+    {
+        char* tmp = strdup(line);
+        //printf("Matching symbol: %s against: %s\n", getfield(tmp, 1), element_name);
+        if (strcmp(getfield(tmp, 1), element_name) == 0)
+        {
+            result = true;
+        }
+        // NOTE strtok clobbers tmp
+        free(tmp);
+    }
+
+    return result;
 }
 
 // return file pointer
@@ -120,19 +152,56 @@ bool test_add_record()
  */
 int main()
 {
-
-    FILE *fp = open_file(ELEMENTS_FILE);
-    struct Element e;
-    strcpy(e.name, "Hydrogen");
-    strcpy(e.symbol, "H");
-    e.atomic_no = 1;
-    e.atomic_wt = 1.008;
-    printf("Add record: %d\n", add_record(&e, fp));
-    printf("File size: %ld (bytes)\n", file_size(fp));
-    printf("Record exists? %d\n", record_exists(&e, fp));
-    fclose(fp);
+    run_tests();
 
     //printf("%d\n", test_add_record());
     getchar();
     return 0;
+}
+
+
+void run_tests() {
+
+    FILE *fp = open_file(ELEMENTS_FILE);
+    struct Element e;
+    strcpy(e.symbol, "H");
+    strcpy(e.name, "Hydrogen");
+    e.atomic_no = 1;
+    e.atomic_wt = 1.008000;
+    printf("Add record: %d\n", add_record(&e, fp));
+    printf("File size: %ld (bytes)\n", file_size(fp));
+    printf("Record exists? %d\n", record_exists(e.symbol, fp));
+
+
+    static const struct Element emptyElement;
+   // e = emptyElement;
+    strcpy(e.symbol, "Ti");
+    strcpy(e.name, "Titanium");
+    e.atomic_no = 22;
+    e.atomic_wt = 47.867000;
+    printf("\nTi exists? %d\n", record_exists(e.symbol, fp));
+    printf("Add record: %d\n", add_record(&e, fp));
+    printf("File size: %ld (bytes)\n", file_size(fp));
+    printf("Ti exists? %d\n", record_exists(e.symbol, fp));
+
+    //e = emptyElement;
+    strcpy(e.symbol, "Ti");
+    strcpy(e.name, "Titanium");
+    e.atomic_no = 22;
+    e.atomic_wt = 47.867000;
+    printf("\nTi exists? %d\n", record_exists(e.symbol, fp));
+    printf("Add record: %d\n", add_record(&e, fp));
+    printf("File size: %ld (bytes)\n", file_size(fp));
+    printf("Ti exists? %d\n", record_exists(e.symbol, fp));
+
+    printf("\nAg exists? %d\n", record_exists("Ag", fp));
+    strcpy(e.symbol, "Ag");
+    strcpy(e.name, "Silver");
+    e.atomic_no = 47;
+    e.atomic_wt = 107.870000;
+    printf("Add record: %d\n", add_record(&e, fp));
+    printf("File size: %ld (bytes)\n", file_size(fp));
+    printf("Ag exists? %d\n", record_exists("Ag", fp));
+
+    fclose(fp);
 }
