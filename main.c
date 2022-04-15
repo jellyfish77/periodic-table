2608998dbad8b2cd3214d31be3253393f9f1bce2
jellyfish77 <jellyfish77@gmx.com>
2022-04-15 10:14:26 +0800

    feat: add inital set of funcs for implementation

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // Structure of the employee
-struct element
+struct Element
 {
     char name[50];
     char symbol[1];
@@ -11,7 +12,29 @@ struct element
     float atomic_wt;
 };
 
-struct element e;
+// Function to add an element record if it doesn't exist (by symbol)
+bool add_record(struct Element e)
+{
+}
+
+// Function to delete an element record matching symbol
+bool delete_record(struct Element e)
+{
+}
+
+// Function to modify an element record matching symbol
+bool modify_record()
+{
+}
+
+// Function to return element record(s) matching all members of e
+bool get_records(struct Element e, bool match_all)
+{
+}
+
+
+
+
 
 /*
  * Function:  main
@@ -24,6 +47,8 @@ int main()
 {
 
     printf("test");
+
+    struct Element e;
 
     return 0;
 }
