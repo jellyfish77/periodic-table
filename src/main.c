12f55270b1cd8004277cb49b54bba79ba545583a
jellyfish77 <jellyfish77@gmx.com>
2022-04-25 10:09:33 +0800

    test: update tests to use set_element func

--- src/main.c
+++ src/main.c
@@ -32,55 +32,31 @@ int main()
 
 void run_tests() {
 
-    static const struct Element emptyElement = {};
+    //static const struct Element emptyElement = {};
 
     FILE *fp = open_file(ELEMENTS_FILE);
-
-    struct Element e2;
-    e2 = set_element("H", "Hydrogen", 1, 1.00800);
-    e2 = set_element("Ti", "Titanium", 22, 47.867000);
-    e2 = set_element("Ag", "Silver", 47, 107.870000);
-
     struct Element e;
-    e = emptyElement; // empty struct
+    //e = emptyElement; // empty struct
 
-    strcpy(e.symbol, "H");
-    strcpy(e.name, "Hydrogen");
-    e.atomic_no = 1;
-    e.atomic_wt = 1.008000;
+    e = set_element("H", "Hydrogen", 1, 1.00800);
     printf("Add record 'H': %d\n", add_record(&e, fp));
     printf("File size: %ld (bytes)\n", file_size(fp));
     printf("Record exists? %d\n", record_exists(e.symbol, fp));
     print_record(&e);
 
-    strcpy(e.symbol, "Ti");
-    strcpy(e.name, "Titanium");
-    e.atomic_no = 22;
-    e.atomic_wt = 47.867000;
+    e = set_element("Ti", "Titanium", 22, 47.867000);
     printf("\nTi exists? %d\n", record_exists(e.symbol, fp));
     printf("Add record 'Ti': %d\n", add_record(&e, fp));
     printf("File size: %ld (bytes)\n", file_size(fp));
     printf("Ti exists? %d\n", record_exists(e.symbol, fp));
-    print_record(&e);
-
-    //e = emptyElement;
-    strcpy(e.symbol, "Ti");
-    strcpy(e.name, "Titanium");
-    e.atomic_no = 22;
-    e.atomic_wt = 47.867000;
-    printf("\nTi exists? %d\n", record_exists(e.symbol, fp));
-    printf("Add record: %d\n", add_record(&e, fp));
+    printf("Add record 'Ti': %d\n", add_record(&e, fp));
     printf("File size: %ld (bytes)\n", file_size(fp));
-    printf("Ti exists? %d\n", record_exists(e.symbol, fp));
 
-    printf("\nAg exists? %d\n", record_exists("Ag", fp));
-    strcpy(e.symbol, "Ag");
-    strcpy(e.name, "Silver");
-    e.atomic_no = 47;
-    e.atomic_wt = 107.870000;
-    printf("Add record: %d\n", add_record(&e, fp));
+    e = set_element("Ag", "Silver", 47, 107.870000);
+    printf("\n'%s' exists? %d\n", e.symbol, record_exists("Ag", fp));
+    printf("Add record '%s': %d\n", e.symbol, add_record(&e, fp));
     printf("File size: %ld (bytes)\n", file_size(fp));
-    printf("Ag exists? %d\n", record_exists("Ag", fp));
+    printf("'%s' exists? %d\n", e.symbol, record_exists("Ag", fp));
 
     fclose(fp);
 }
